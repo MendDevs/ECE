50be2dd1b862f91948e71a2616c829b9712baea3
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2025-09-12 05:15:44 +0200

    Update exo1.c

--- Operating Systems/TP1/exo1.c
+++ Operating Systems/TP1/exo1.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 
 int main(int argc, char **argv){
+    //counts the number of arguments
 
    if(argc !=2) return 1;
 
@@ -11,7 +12,16 @@ int main(int argc, char **argv){
         return 1;
    }
 
+    printf("[stat] File Info of %s\n" , argv[1]);
+    printf("--------------------------------------\n");
 
+    printf("Inode Number: \t\t%d\n", fileInfo.st_ino); //1. Inode number
+    printf("Total Links : \t\t%d\n", fileInfo.st_nlink); //2. #of link
+
+    printf("");//its owners
+    //groups
+    //size
+    //type
 
 return 0;
 }
