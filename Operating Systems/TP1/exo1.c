b81d597b7faf3be25095d3b16c77d45530ce8f0a
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2025-09-12 06:06:25 +0200

    Update exo1.c

--- Operating Systems/TP1/exo1.c
+++ Operating Systems/TP1/exo1.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h> //for calling low level unix functions
+#include<pwd.h>  //for getting user name
+#include<grp.h>  //for getting group name
 
 int main(int argc, char **argv){
     //counts the number of arguments
 
-   if(argc !=2) return 1;
+    //check if exactly 1 argument (filename) is given
+   if(argc !=2){
+        printf("Usage: %s<filename>\n",argv[0]);
+    return 1;}
 
    struct stat fileInfo;
+
+   //Get file info
    if(stat(argv[1], &fileInfo)<0){
         perror("stat failed");
         return 1;
@@ -15,13 +24,32 @@ int main(int argc, char **argv){
     printf("[stat] File Info of %s\n" , argv[1]);
     printf("--------------------------------------\n");
 
-    printf("Inode Number: \t\t%d\n", fileInfo.st_ino); //1. Inode number
-    printf("Total Links : \t\t%d\n", fileInfo.st_nlink); //2. #of link
+    //1. Inode number
+    printf("Inode Number: \t\t%d\n", fileInfo.st_ino);
 
-    printf("");//its owners
-    //groups
-    //size
-    //type
+    //2. #of link
+    printf("Total Links : \t\t%d\n", fileInfo.st_nlink);
 
-return 0;
+    //3. Owner (user)
+    struct passwd *pw = getpwuid(fileInfo.st_uid);
+    if (pw != NULL)
+        printf("Owner: \t\t\t%s\n", pw->pw_name);
+    else
+        printf("Owner UID: \t\t%d\n", fileInfo.st_uid);
+
+    //4. Group
+    struct  group *grp = getgrgid(fileInfo.st_gid);
+    if(grp != NULL)
+        printf("Group GID: \t\t%d\n", grp->gr_name);
+    else
+        printf("Group GID: \t\t%d\n", fileInfo.st_gid);
+
+    //5. Size
+
+
+    //6. File Type
+
+    return 0;
+}
+
 }
