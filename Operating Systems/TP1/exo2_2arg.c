5e915044689248f9c66fe380a07f43185f8e9904
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2025-09-13 09:59:10 +0200

    Update exo2_2arg.c

--- Operating Systems/TP1/exo2_2arg.c
+++ Operating Systems/TP1/exo2_2arg.c
@@ -10,8 +10,8 @@ int main(int argc[],char **argv){
 
         //incorrect number of parameters
         if(argc !=3 ){
-            printf("Error: Incorrect number of parameters");
-            printf("Usage: %s <file1 name> <file2 name>",argv[0]);
+            printf("Error: Incorrect number of parameters \n");
+            printf("Usage: %s <file1 name> <file2 name> \n",argv[0]);
             return -1;
         }
 
@@ -19,14 +19,14 @@ int main(int argc[],char **argv){
 
         //primitive stat error
         if((stat(argv[1], &file1) < 0 )|| (stat(argv[2], &file2)<0)){
-            perror("Stat Error");
+            perror("Stat Error \n");
             return 2;
         }
 
         //same node
         else if (file1.st_ino==file2.st_ino &&
             file1.st_dev==file2.st_dev){
-            printf("Identical files");
+            printf("Identical files \n");
             return 1;
         }else{
             //different nodes
