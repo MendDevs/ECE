b03dd99e97f58486c37572cb3497f35548f6ac84
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2025-09-16 14:53:15 +0200

    Update example_sig.c

--- Operating Systems/Examples/example_sig.c
+++ Operating Systems/Examples/example_sig.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+void sighandler(int signum)
+{
+    printf("Masquage du signal SIGTERM\n");
+}
+
+int main(void){
+    char buffer[256];
+    if(signal(SIGTERM,&sighandler)== SIG_ERR)
+    {
+        printf("Ne peut pas manipuler le signal'n");
+        exit(1);
+    }
+    while(1){
+        fgets(buffer,sizeof(buffer), stdin);
+        printf("Input: %s\n",buffer);
+    }
+
+}
 
