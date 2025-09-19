0e500e789e173cfe309f1e4bc3bd95027f61317e
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-09-19 14:20:59 +0200

    added lab 2 to Machine Learning.

---
+++ Data Science/Cours/exp_conversion.py
@@ -0,0 +1,18 @@
+
+import math
+
+i = math.ceil(5.834)
+print(i)
+
+i = math.floor(5.834)
+print(i)
+
+i = round(5.834)
+print(i)
+
+####### The differene b/w round(), floor() and 
+######   ceil()
+
+# round() -> rounds to the nearest (ties to even)
+# floor() -> always down.
+# ceil() -> always up
