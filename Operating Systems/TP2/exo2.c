43ec94368b0a464629e26adbc4b572effb05d62f
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2025-09-21 21:32:37 +0200

    Update exo2.c

--- Operating Systems/TP2/exo2.c
+++ Operating Systems/TP2/exo2.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+int main(){
+    pid_t pid1 = fork();
+
+    if (pid1 == 0){ // fils 1
+        for(int i = 1; i<=50; i++){
+            printf("%d ", i);
+        }
+        printf("\n");
+        exit(0);
+    }else{
+        pid_t pid2 = fork();
+        if(pid2 == 0){ // fils2
+        for(int i = 51; i <= 100; i++){
+            printf("%d ", i);
+        }
+         printf("\n");
+         exit(0);
+        }
+    }
+
+    // version modified:
+    /*
+        pid_t pid1 = fork();
+    if (pid1 == 0) {
+        for (int i = 1; i <= 50; i++) printf("%d ", i);
+        exit(0);
+    } else {
+        wait(NULL); // attendre le fils 1
+        pid_t pid2 = fork();
+        if (pid2 == 0) {
+            for (int i = 51; i <= 100; i++) printf("%d ", i);
+            exit(0);
+        }
+        wait(NULL);
+    }
+    */
+
+    return 0;
+}
