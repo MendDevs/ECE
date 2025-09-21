670a55ebdff04af23f03cd2384339e7f18d48d50
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2025-09-21 21:43:48 +0200

    Update exo2.c

--- Operating Systems/TP2/exo2.c
+++ Operating Systems/TP2/exo2.c
@@ -6,15 +6,19 @@
 int main(){
     pid_t pid1 = fork();
 
+
     if (pid1 == 0){ // fils 1
+    printf("Count Process 1\n");
         for(int i = 1; i<=50; i++){
             printf("%d ", i);
         }
-        printf("\n");
+        printf("\n\n");
         exit(0);
     }else{
+
         pid_t pid2 = fork();
         if(pid2 == 0){ // fils2
+            printf("Count Process 2\n");
         for(int i = 51; i <= 100; i++){
             printf("%d ", i);
         }
