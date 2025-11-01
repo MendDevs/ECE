9a2d813807ca7bc5dc793eebe771eb658beb8104
MendDevs <morrisemmanuelmarkie@gmail.com>
2025-11-01 17:08:07 +0100

    progress

--- Machine Learning/Lab 3/tp3.py
+++ Machine Learning/Lab 3/tp3.py
@@ -1,11 +1,30 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np
-
+import io
 #################### Page configuration #########
 st.set_page_config(layout="wide")
 st.title("TP 3 - Classification supervisé")
 st.markdown("""----""")
 ###################### partie 1 #################
+st.markdown("## Analyse Préliminaire")
 
+#loading data
 titanic_train = pd.read_csv("titanic_train.csv")
+st.markdown("### Data Display")
+st.dataframe(titanic_train)
+
+
+st.markdown("### Preliminary Analysis")
+
+#Attributs:
+st.markdown("#### 1. General Information")
+with st.expander("Click me to display an overview of the data"):
+    info_titanic_train = pd.DataFrame({
+        "Colonnes" : titanic_train.columns,
+        "Type"     : titanic_train.dtypes.astype(str),
+        "Valeurs non nulles" : titanic_train.notnull().sum(),
+        "Valeurs nulles" : titanic_train.isnull().sum(),
+        "Pourcentage Manquant (%)" : round(titanic_train.isnull().mean()*100,2)
+    })
+    st.dataframe(info_titanic_train)
